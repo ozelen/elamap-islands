264d87f501e68fc7a2d99b200f86a19e1893d776
Oleksiy Zelenyuk <oleksadesign@gmail.com>
2013-03-25 13:04:54 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ElamapIslands::Application.config.secret_token = 'fc42eed44fe05d4229ed75c03d95dd6524ba051640c3a394e638ed01603a8d2a571fe6d1168dbaf4807a5e93a8a969047a21e65291711dd42fe23c2eb28c2a71'
