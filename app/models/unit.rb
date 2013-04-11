189b910e7127956282465cbae11dd1a71ef98de6
Oleksiy Zelenyuk <ozelen@wserver.(none)>
2013-04-11 21:52:51 +0400

    fix changing sequence on Text edit

--- app/models/unit.rb
+++ app/models/unit.rb
@@ -1,5 +1,5 @@
 class Unit < ActiveRecord::Base
-  has_many :texts, :dependent => :destroy
+  has_many :texts, :dependent => :destroy, :order => "sequence ASC"
   belongs_to :session
   attr_accessible :name, :texts, :letter, :session, :session_id
 end
