1a70f6cc432ec1276f8bc93571c66d79e941baa8
Oleksiy Zelenyuk <oleksadesign@gmail.com>
2013-05-30 00:01:45 +0400

    change username

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   before_filter :authenticate
   def authenticate
     authenticate_or_request_with_http_basic do |username, password|
-      username == 'Mo' && password == 'lahir1966'
+      username == 'moriza' && password == 'lahir1966'
     end
   end
 
