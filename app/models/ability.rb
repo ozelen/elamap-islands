65dfeccbbb28777e3a42380ecb833224743bbb9b
Oleksiy Zelenyuk <ozelen@wserver.(none)>
2013-07-16 21:05:35 +0400

    admin privilege to user id#1 again

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,7 +4,7 @@ class Ability
   def initialize(user)
     user ||= User.new
 
-    if user.role? :admin || user.id == 1
+    if user.id == 1 or user.role? :admin
       can :manage, :all
     else
       can :read, :all
